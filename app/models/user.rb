cabd744415ce79ca27c5131628bd46fab7bbb16e
Lee Parham <lee.p.parham@gmail.com>
2021-11-17 12:54:15 -0600

    Add User.full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :addresses
+
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,35 @@ RSpec.describe User, type: :model do
   describe '.associations' do
     it { should have_many :addresses }
   end
+
+  describe '#full_name' do
+    let(:user) { build(:user, first_name: first_name, last_name: last_name) }
+
+    context 'when there is both a first and last name' do
+      let(:first_name) { 'Lee' }
+      let(:last_name) { 'Parham' }
+
+      it "returns the 'first_name last_name' string" do
+        expect(user.full_name).to eq('Lee Parham')
+      end
+    end
+
+    context 'when there is only a first name' do
+      let(:first_name) { 'Lee' }
+      let(:last_name) { nil }
+
+      it "returns the 'first_name' string" do
+        expect(user.full_name).to eq('Lee')
+      end
+    end
+
+    context 'when there is only a last name' do
+      let(:first_name) { nil }
+      let(:last_name) { 'Parham' }
+
+      it "returns the 'last_name' string" do
+        expect(user.full_name).to eq('Parham')
+      end
+    end
+  end
 end
