f44811164a00f6f03726ce4ccd205a31eab25fb2
Lee Parham <lee.p.parham@gmail.com>
2021-11-17 13:29:12 -0600

    Use xml_body

--- app/interactors/build_query_string.rb
+++ app/interactors/build_query_string.rb
@@ -3,14 +3,14 @@ class BuildQueryString
 
   # Accepts and address and returns the xml_query
   def call
-    context.xml_query = build_query_string.squish.gsub('> <', '><')
+    context.xml_body = build_query_string.squish.gsub('> <', '><')
   end
 
   private
 
   def build_query_string
     <<~XML
-      <AddressValidateRequest  USERID=#{Rails.application.credentials.usps[:user]}>
+      <AddressValidateRequest  USERID='#{Rails.application.credentials.usps[:user]}'>
         <Revision>1</Revision>
         <Address ID='0'>
           <Address1>#{user.full_name}</Address1>
